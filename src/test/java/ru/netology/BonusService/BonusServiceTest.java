2bca557e5da55a8fa2866e44d9b0462108fcd5c7
Сергей Душенбиев <agnium@mail.ru>
2022-02-24 16:24:14 +0300

    Java 6.2 refactored

--- src/test/java/ru/netology/BonusService/BonusServiceTest.java
+++ src/test/java/ru/netology/BonusService/BonusServiceTest.java
@@ -1,6 +1,7 @@
+package ru.netology.BonusService;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BonusServiceTest {
